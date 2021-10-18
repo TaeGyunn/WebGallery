2284dd3403ffbb368ebc846a97c0086d561f029b
TaeGyun <taekuen98@naver.com>
2021-10-18 10:49:11 +0900

    21.10.18 LoginDTO

---
+++ src/main/java/WebGallery/Gallery/dto/LoginDTO.java
@@ -0,0 +1,19 @@
+package WebGallery.Gallery.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+@Getter
+@Setter
+public class LoginDTO {
+
+    @NotBlank(message = "아이디를 입력해주세요")
+    private String id;
+
+    @NotBlank(message = "비밀번호를 입력해주세요.")
+    private String pw;
+
+}
