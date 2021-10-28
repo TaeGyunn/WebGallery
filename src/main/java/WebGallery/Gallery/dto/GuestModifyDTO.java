77aa3b215d83d63184fb436fc2f4fb7606baaec7
TaeGyun <taekuen98@naver.com>
2021-10-28 20:17:35 +0900

    21.10.28 Guest Modify

---
+++ src/main/java/WebGallery/Gallery/dto/GuestModifyDTO.java
@@ -0,0 +1,24 @@
+package WebGallery.Gallery.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Size;
+
+@Getter
+@Setter
+public class GuestModifyDTO {
+
+    private Long gno;
+
+    @Size(min = 2, max = 10, message = "닉네임은 2자 이상 10자 이하로 입력해주세요.")
+    private String nick;
+
+    @Size(min = 8, max = 20, message = "비밀번호는 8자 이상 20자 이하로 입력해주세요.")
+    private String pw;
+
+    @Email(message = "올바른 이메일 주소를 입력해주세요.")
+    private String email;
+
+}
