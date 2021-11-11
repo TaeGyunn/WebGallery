445869a5bb944283b1827aa8903e607f8200bd97
TaeGyun <taekuen98@naver.com>
2021-11-11 15:37:20 +0900

    21.11.11

--- src/main/java/WebGallery/Gallery/GalleryApplication.java
+++ src/main/java/WebGallery/Gallery/GalleryApplication.java
@@ -9,7 +9,7 @@ public class GalleryApplication {
 
 	private static final String PROPERTIES =
 			"spring.config.location="
-			+"classpath:/application.properties"
+			+"classpath:/application.yml"
 			+",/home/ec2-user/app/application-security.yml";
 
 	public static void main(String[] args) {
