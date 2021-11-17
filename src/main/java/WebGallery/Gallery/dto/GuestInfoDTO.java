429ed560711b4333aa6fc32816ad7d6c9c8a22b3
TaeGyun <taekuen98@naver.com>
2021-11-17 13:31:28 +0900

    21.11.17

--- src/main/java/WebGallery/Gallery/dto/GuestInfoDTO.java
+++ src/main/java/WebGallery/Gallery/dto/GuestInfoDTO.java
@@ -4,11 +4,15 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
 
 @Getter
 @Setter
 @NoArgsConstructor
-public class GuestInfoDTO {
+public class GuestInfoDTO implements UserDetails {
 
     private String nickname;
     private Long gno;
@@ -21,5 +25,40 @@ public class GuestInfoDTO {
 
     public GuestInfoDTO(Object details) {
         this.f5 = details.toString();
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getPassword() {
+        return null;
+    }
+
+    @Override
+    public String getUsername() {
+        return null;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return false;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return false;
     }
 }
