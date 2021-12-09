c33bb8d6dd37b1a70da1fb59bf6672fa627ed50f
TaeGyun <taekuen98@naver.com>
2021-12-09 16:48:28 +0900

    21.12.09

--- src/main/java/WebGallery/Gallery/service/WorkService.java
+++ src/main/java/WebGallery/Gallery/service/WorkService.java
@@ -204,11 +204,6 @@ public class WorkService {
             return response.fail("해당하는 유저가 존재하지 않습니다", HttpStatus.BAD_REQUEST);
         }
         Work work = workRepository.findByWno(wno);
-        if(work.getLikes()== null){
-            log.info("nullllllll");
-            return response.fail("테스트", HttpStatus.BAD_REQUEST);
-
-        }
 
         if(work == null){
             return response.fail("해당하는 작품이 존재하지 않습니다", HttpStatus.BAD_REQUEST);
@@ -216,14 +211,14 @@ public class WorkService {
         boolean check = likeRepository.existsByGuestAndWork(guest, work);
 
         if(check){
-            work.changeLike(work.getLikes());
+            work.changeLike(work.getLikes()-1);
             workRepository.save(work);
             Likes likes = likeRepository.findByGuestAndWork(guest,work);
             likeRepository.delete(likes);
             return response.success("아이템 좋아요 취소 성공");
 
         }else{
-            work.changeLike(work.getLikes());
+            work.changeLike(work.getLikes()+1);
             workRepository.save(work);
             Likes likes = new Likes(work, guest);
             likeRepository.save(likes);
