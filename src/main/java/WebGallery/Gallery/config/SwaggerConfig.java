9aee051fae75138a43074b9de0cda000f15c8414
TaeGyun <taekuen98@naver.com>
2021-12-12 22:59:20 +0900

    21.12.12

--- src/main/java/WebGallery/Gallery/config/SwaggerConfig.java
+++ src/main/java/WebGallery/Gallery/config/SwaggerConfig.java
@@ -5,26 +5,22 @@ import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.ApiKey;
-import springfox.documentation.service.AuthorizationScope;
-import springfox.documentation.service.SecurityReference;
+import springfox.documentation.service.*;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import java.util.Arrays;
 import java.util.List;
 
 @Configuration
-@EnableSwagger2
 public class SwaggerConfig {
 
     @Bean
     public Docket swaggerApi() {
 
         return new Docket(DocumentationType.OAS_30)
+                .useDefaultResponseMessages(false)
                 .securityContexts(Arrays.asList(securityContext()))
                 .securitySchemes(Arrays.asList(apiKey()))
                 .select()
@@ -38,6 +34,7 @@ public class SwaggerConfig {
         return new ApiInfoBuilder()
                 .title("WGallery API")
                 .description("WGallery API Info")
+                .contact(new Contact("TaeGyun", "https://github.com/TaeGyunn/WebGallery.git","cjstkzlxl98@gmail.com"))
                 .build();
     }
 
