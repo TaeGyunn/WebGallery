66dce78f038dfe651cbbc9e04316b93670b1424b
TaeGyun <taekuen98@naver.com>
2021-12-12 23:06:25 +0900

    21.12.12

--- src/main/java/WebGallery/Gallery/config/SwaggerConfig.java
+++ src/main/java/WebGallery/Gallery/config/SwaggerConfig.java
@@ -9,17 +9,19 @@ import springfox.documentation.service.*;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import java.util.Arrays;
 import java.util.List;
 
 @Configuration
+@EnableSwagger2
 public class SwaggerConfig {
 
     @Bean
     public Docket swaggerApi() {
 
-        return new Docket(DocumentationType.OAS_30)
+        return new Docket(DocumentationType.SWAGGER_2)
                 .useDefaultResponseMessages(false)
                 .securityContexts(Arrays.asList(securityContext()))
                 .securitySchemes(Arrays.asList(apiKey()))
