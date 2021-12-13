9c7fa07bc8e237c41c25e8298a4b7a3508a0e264
TaeGyun <taekuen98@naver.com>
2021-12-13 11:48:03 +0900

    21.12.13

--- src/main/java/WebGallery/Gallery/controller/workController.java
+++ src/main/java/WebGallery/Gallery/controller/workController.java
@@ -72,7 +72,7 @@ public class workController {
     
     //작업물 추가
     @ApiOperation(value="작업물 추가", notes = "작가가 작업물을 추가한다.")
-    @PostMapping("/author/insertWork")
+    @PostMapping(path = "/author/insertWork", consumes = {"multipart/form-data"})
     public ResponseEntity<?> insertWork(@RequestPart("insert") InsertWorkDTO insertWorkDTO,
                                      @RequestPart("photo") MultipartFile photo){
 
