ceb726ca29c0bea0d06b8c8035a71a0cbe7e142c
TaeGyun <taekuen98@naver.com>
2022-01-18 16:20:25 +0900

    21.01.18

---
+++ src/main/java/WebGallery/Gallery/config/TomcatWebServerCustomizer.java
@@ -0,0 +1,14 @@
+package WebGallery.Gallery.config;
+
+import org.springframework.boot.web.embedded.tomcat.TomcatConnectorCustomizer;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
+
+public class TomcatWebServerCustomizer implements WebServerFactoryCustomizer<TomcatServletWebServerFactory> {
+
+    @Override
+    public void customize(TomcatServletWebServerFactory factory) {
+        factory.addConnectorCustomizers((TomcatConnectorCustomizer)
+                connector -> connector.setAttribute("relaxedQueryChars", "<>[\\]^`{|}"));
+    }
+}
